33f494c3fbed42c371e7aa62c69123edc0d14895
Winston La <winston.la@atomworks.io>
2024-05-12 00:15:32 +0700

    Auto stash before cherry pick of "fake session"

--- market-core/src/main/java/market/interceptor/FakeAuthenInterceptor.java
+++ market-core/src/main/java/market/interceptor/FakeAuthenInterceptor.java
@@ -3,6 +3,7 @@ package market.interceptor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,11 @@ public class FakeAuthenInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		Authentication auth = new UsernamePasswordAuthenticationToken("admin", "password");
+		String username = request.getHeader("username");
+		if (!StringUtils.hasText(username)){
+			username = "admin";
+		}
+		Authentication auth = new UsernamePasswordAuthenticationToken(username, "password");
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		return super.preHandle(request, response, handler);
 	}
